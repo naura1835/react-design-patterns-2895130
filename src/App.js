69771e3611d230baeef9188157acb96c7c2a8950
Khadija Gwarzo <khadijagarbag@gmail.com>
2023-11-15 11:11:09 +0100

    follow along HOC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
+import { withUser } from "./withUser";
+
+const UserInfoWrapper = withUser(UserInfo, 234);
+
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return <UserInfoWrapper />;
 }
 
 export default App;
